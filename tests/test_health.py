cf935d9313f39a272b6b36b289b9f948667ff4d7
MikoŁaj Kocik <mikolajkocik@onet.pl>
2026-09-03 21:49:05 +0200

    tests: add initial test for API  health check

---
+++ tests/test_health.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from src.api.main import app
+
+client = TestClient(app)
+
+def test_health_check_returns_ok():
+    """
+    Should return ok (200) if service is available
+    """
+    # Act
+    response = client.get("/health")
+    
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "ok", 
+        "service": "ner-disease-classification-gateway"
+    }
